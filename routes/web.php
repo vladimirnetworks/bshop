e0cbf0f5fa681192e6dc4adfcb01a55c667c79fa
vladimirnetworks <vladimirnetworks@gmail.com>
2021-11-21 10:23:26 +0330

    update using fastpush

--- routes/web.php
+++ routes/web.php
@@ -15,7 +15,7 @@ use Illuminate\Support\Facades\Route;
 
 
 
-Route::get('/', "App\Http\Controllers\mainPageController@index");
+Route::get('/', "App\Http\Controllers\mainPageController@index")->middleware('tokin');
 
 
 Route::get('/product/{product}', "App\Http\Controllers\ProductController@show")->middleware('tokin');
