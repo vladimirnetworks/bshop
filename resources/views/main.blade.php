e24bb2f137211e474bfbbe5e090cc64b42b5147b
vladimirnetworks <vladimirnetworks@gmail.com>
2021-11-21 14:04:44 +0330

    update using fastpush

--- resources/views/main.blade.php
+++ resources/views/main.blade.php
@@ -307,7 +307,7 @@ console.log('called');
 
       var elem  = $('<div class="p-2 row"></div>');
 
-      var tedad = $('<input class="col-1 text-center" type="number">').css({"width":"40px","text-align":"center"}).on('click touchstart',function(e){
+      var tedad = $('<input style="direction:ltr" class="col-1 text-center" type="number">').css({"width":"40px","text-align":"center"}).on('click touchstart',function(e){
             e.preventDefault();
             e.stopPropagation();
        return true;
