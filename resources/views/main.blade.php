902ad6174b98bbda52407a4b988f19d6e07a2dd0
vladimirnetworks <vladimirnetworks@gmail.com>
2021-11-21 21:31:20 +0330

    update using fastpush

--- resources/views/main.blade.php
+++ resources/views/main.blade.php
@@ -69,7 +69,7 @@ behkiana - phone : 066-42448787
 لطفا شماره تماستون رو وارد کنید 
  <br>
  <div class="row p-3">
-<form id="reggetnumber" name="reggetnumberform" id="reggetnumber" action="" method="get">
+<form id="reggetnumber" name="reggetnumberform"  action="/" method="post">
 <input class="form-control col-10" style="font-size:24px;" type="number" id="getnumber" placeholder="شماره تماس"> 
 <button type="submit" class="btn btn-success col-2" data-dismiss="modal">ثبت</button> 
 </form>
