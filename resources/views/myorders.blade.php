ba434a6e2ecf855c24cf929cd14f867224eaa0a1
vladimirnetworks <vladimirnetworks@gmail.com>
2021-11-23 10:23:30 +0330

    update using fastpush

--- resources/views/myorders.blade.php
+++ resources/views/myorders.blade.php
@@ -4,22 +4,22 @@
 
 <h1>سفارشات من</h1>
 
-<!-- 
+
 
 <div class="container-fluid" >
 
     <div class="row">
     @foreach($orders as $order)
 
-<div class="border">
-      {{$order->data}}
-</div>
+    <div class="border">
+      {{implode("<br>",$order->titles)}}
+    </div>
     @endforeach
     </div>
 
 </div>
 
--->
+
 
 
 
