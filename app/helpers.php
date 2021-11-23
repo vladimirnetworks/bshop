073e9cd1e6ab856a48899d73d0872feb8fa04716
vladimirnetworks <vladimirnetworks@gmail.com>
2021-11-23 14:43:08 +0330

    update using fastpush

--- app/helpers.php
+++ app/helpers.php
@@ -1,14 +1,43 @@
 
    <?php
-   
-    function encode_id($inp)
-    {
 
-        $begin = rand(1,9);
-        $end = rand(1,9);      
-        $xr = $begin.strrev($inp+$end+$begin).$end;
+function encode_id($inp)
+{
 
-        return $xr;
+    $begin = rand(1, 9);
+    $end = rand(1, 9);
+
+    $makhfil = $inp + $end + $begin;
+
+
+    if ($begin * $end % 2 == 0) {
+
+        $makhfil = strrev($makhfil);
     }
 
-    ?>
+    $xr = $begin . (($makhfil)) . $end;
+
+    return $xr;
+}
+
+function decode_id($inp)
+{
+    $begin = substr($inp, 0, 1);
+    $end = substr($inp, strlen($inp) - 1, 1);
+
+
+    $makhfil = substr($inp, 1, strlen($inp) - 2);
+   
+    if ($begin * $end % 2 == 0) {
+
+        $makhfil = strrev($makhfil);
+    }
+
+    $number = $makhfil-$begin-$end;
+    return   $number;
+}
+
+
+
+
+?>
