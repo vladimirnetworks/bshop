6505649233c62d8b993fb926b6b1d55b4b80b3a9
vladimirnetworks <vladimirnetworks@gmail.com>
2021-11-24 21:16:20 +0330

    update using fastpush

--- resources/views/productItem.blade.php
+++ resources/views/productItem.blade.php
@@ -1,12 +1,11 @@
 <div class="col-sm-6 col-lg-3 p-2 text-center">
 
 <div class="border border-secondary h-100 rounded">
-<a href="product/{{$product->id}}" class="d-block"><img class="mw-100" src="{{$product->thumb}}"></a>
          
          <a href="product/{{$product->id}}" class="d-block">{{$product->title}}</a>
 
 <br>
-      <a href="product/{{$product->id}}">  <img class="mw-100" src="https://shopid.ir/De-Ordiner-Skin-Serum-Model-Lactic-Acid-Volume-30-ml-2.jpg"/></a>
+      <a href="product/{{$product->id}}">  <img class="mw-100" src="{{$product->photos}}"/></a>
 
          {{number_format($product->price)}} تومان
 
