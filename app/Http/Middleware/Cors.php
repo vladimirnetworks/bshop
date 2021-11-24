31bd51d953550cdf710bb8974f5d5f9dd689acb1
vladimirnetworks <vladimirnetworks@gmail.com>
2021-11-24 20:39:32 +0330

    update using fastpush

--- app/Http/Middleware/Cors.php
+++ app/Http/Middleware/Cors.php
@@ -17,9 +17,9 @@ class Cors
     public function handle(Request $request, Closure $next)
     {
 
-        header('Access-Control-Allow-Origin: *');
-        header('Access-Control-Allow-Methods: *');
-        header('Access-Control-Allow-Headers: *');
+       // header('Access-Control-Allow-Origin: *');
+      ///  header('Access-Control-Allow-Methods: *');
+      //  header('Access-Control-Allow-Headers: *');
 
         return $next($request);
        // ->header('Access-Control-Allow-Origin', '*')
