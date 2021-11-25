d689090eddb44d451d570cd94e3c5202025c4f39
vladimirnetworks <vladimirnetworks@gmail.com>
2021-11-25 14:51:16 +0330

    update using fastpush

--- resources/views/login.blade.php
+++ resources/views/login.blade.php
@@ -11,18 +11,18 @@
 
 <div class="container-fluid text-center border rounded p-3 ">
 
- <div class="row w-70">
+ <div class="row">
     <div class="col text-left">نام کاربری</div>
     <div class="col text-right"><input name="user" type="text" class="form-control"></div>
  </div>
 
-  <div class="row w-70">
+  <div class="row ">
     <div class="col text-left">رمز عبور</div>
     <div class="col text-right"><input name="pass" type="password" class="form-control"></div>
  </div>
 
 
- <button class="btn btn-primary m-3">ورود</button>
+ <button class="btn btn-primary m-3 p-3">ورود</button>
 </div>
 
 </form>
