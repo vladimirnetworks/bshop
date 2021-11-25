711e6e4564f72da8790b60c74fbad32e708e5941
vladimirnetworks <vladimirnetworks@gmail.com>
2021-11-25 09:19:01 +0330

    update using fastpush

--- resources/views/singleProduct.blade.php
+++ resources/views/singleProduct.blade.php
@@ -4,14 +4,19 @@
 
 <h1>{{$product->title}}</h1>
 
+<div class="row">
+<div class="col-8">
 <div>
  <img style="width:200px;" src="{{$product->photos}}" style="max-width:100%"><br>
+
+<h4> مشخصات محصول 
+</h4>
 <ul>
 {!!lize($product->caption)!!}
 </ul>
 </div>
-
-
+</div>
+<div class="col-4">
 قیمت : {{number_format($product->price)}}
 
 <!--
@@ -19,6 +24,7 @@
 -->
 <br>
  <button type="button" class="btn btn-success btn-lg addtocart" data-prod="{{$product->cartnfo()}}" >خرید</button>
-
+</div>
+</div>
 
 @stop
