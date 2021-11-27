fbbd33b9dea32b8c5591d67205ffcc093b747f89
vladimirnetworks <vladimirnetworks@gmail.com>
2021-11-27 21:54:28 +0330

    update using fastpush

--- resources/views/main2.blade.php
+++ resources/views/main2.blade.php
@@ -529,13 +529,18 @@ $(this).unbind('click');
 });
 </script>
 <style>
-.bar {
-
+.bar:after {
+    content:"";
+    position:absolute;
+    width:100%;
+    bottom:1px;
+    z-index:-1;
+    transform:scale(.9);
     box-shadow: 0px 0px 8px 2px #000000;
 }
 
 </style>
-<div style="position:fixed;height:5vh;background-color:white;top:0px;left:0px;text-align:center" class="row bar">
+<div style="position:fixed;height:5vh;background-color:white;top:0px;left:0px;text-align:center;width:100%" class="row bar">
 
 behkiana
 
