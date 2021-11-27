a7f51141d65c0a6edf88d6ae214c186eb5fb0659
vladimirnetworks <vladimirnetworks@gmail.com>
2021-11-27 16:37:43 +0330

    update using fastpush

--- resources/views/main2.blade.php
+++ resources/views/main2.blade.php
@@ -247,7 +247,7 @@ animate(document.documentElement.scrollTop,$(this).offset().top-50,200,function 
 });
 
 
-
+$(this).html("")
 var price = $('<div class="p-3 text-success" style="font-size:150%;font-weight:bold">18,000 تومان</div>');
 $(this).append(price);
 
