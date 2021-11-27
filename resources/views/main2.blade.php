5e1d1c8064e457b021dbc8f84be59516d31dbf89
vladimirnetworks <vladimirnetworks@gmail.com>
2021-11-27 19:10:45 +0330

    update using fastpush

--- resources/views/main2.blade.php
+++ resources/views/main2.blade.php
@@ -491,9 +491,9 @@ var vals = JSON.parse($(this).attr("data-me"));
 var mySwipe = new SwiperBox({
 					items:
 						[
-						'<div class="myitem"><img src="https://raw.githubusercontent.com/vladimirnetworks/SwiperBox/master/potato.jpg"  /></div>',
-						'<div class="myitem"><img src="https://raw.githubusercontent.com/vladimirnetworks/SwiperBox/master/tomato.jpg"  /></div>',
-						'<div class="myitem"><img src="https://raw.githubusercontent.com/vladimirnetworks/SwiperBox/master/jason2.jpg"  /></div>'
+						'<div class="myitem"><img src="https://www.behkiana.ir/photos/55504679.jpg"  /></div>',
+						'<div class="myitem"><img src="https://www.behkiana.ir/photos/55504679.jpg"  /></div>',
+						'<div class="myitem"><img src="https://www.behkiana.ir/photos/55504679.jpg"  /></div>'
 						]
 						
 					});
