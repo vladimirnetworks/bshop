8dc03bb742511c893c70134ab3161d138bf267b4
vladimirnetworks <vladimirnetworks@gmail.com>
2021-11-27 09:55:08 +0330

    update using fastpush

--- resources/views/productItem.blade.php
+++ resources/views/productItem.blade.php
@@ -1,11 +1,15 @@
 <div class="col-4 col-sm-3  p-1 text-center">
 
-<div class="border border-secondary h-100 rounded">
+<div class="border border-secondary h-100 rounded" style="direction:rtl">
          
-         <a href="product/{{$product->id}}" class="d-block">{{$product->title}}</a>
+
+         <a href="product/{{$product->id}}">  <img class="mw-100" src="{{$product->photo}}"/></a>
+       
 
 <br>
-      <a href="product/{{$product->id}}">  <img class="mw-100" src="{{$product->photo}}"/></a>
+      
+
+        <a href="product/{{$product->id}}" class="d-block">{{$product->title}}</a>
 
          {{number_format($product->price)}} تومان
 
