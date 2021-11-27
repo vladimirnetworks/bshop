fc3db759a43e9b362db8cad4e154c7623cc45c90
vladimirnetworks <vladimirnetworks@gmail.com>
2021-11-27 10:57:52 +0330

    update using fastpush

--- resources/views/productItem.blade.php
+++ resources/views/productItem.blade.php
@@ -1,6 +1,6 @@
-<div class="col-4 col-sm-3  p-1 text-center"  style="flex-direction:column;display:flex">
+<div class="col-4 col-sm-3  p-1 text-center"  >
 
-<div class=" h-100 " style="direction:rtl;background-color:red;">
+<div class=" h-100 " style="direction:rtl;background-color:red;flex-direction:column;display:flex">
          
 
          <a href="product/{{$product->id}}">  <img class="mw-100" src="{{$product->photo}}"/></a>
