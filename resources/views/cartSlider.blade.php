7c0ea55ef82ea2ba11f4006ac80525b3fabcab60
vladimirnetworks <vladimirnetworks@gmail.com>
2021-11-28 17:05:02 +0330

    update using fastpush

--- resources/views/cartSlider.blade.php
+++ resources/views/cartSlider.blade.php
@@ -3,7 +3,7 @@
 </div>
 
 
-<div style="direction:rtl;position:fixed;bottom:0px;left:0px;height:10vh;width:100%;background-color:white;z-index:9999" id="mcartslider" class="text-center cartslider p-3 border-top  p-3  bg-white">
+<div style="direction:rtl;position:fixed;bottom:0px;left:0px;height:10vh;width:100%;background-color:white;z-index:9999" id="mcartslider" class="text-center cartslider p-3 border-top  p-3 ">
  <div class="cartslider_smallview">
  سبد خرید [<span class="cartslider_smallview_text"></span>]
  <button class="btn btn-info m-2">
--- resources/views/main2.blade.php
+++ resources/views/main2.blade.php
@@ -264,17 +264,17 @@ body {
 
 @keyframes xshake_anim {
 
-  0% { transform: translate(1px, 1px) rotate(0deg); }
+  0% { transform: translate(1px, 1px) rotate(0deg);  background-color:white}
   10% { transform: translate(-1px, -2px) rotate(-1deg); }
-  20% { transform: translate(-3px, 0px) rotate(1deg); }
+  20% { transform: translate(-3px, 0px) rotate(1deg); background-color:#fc0073}
   30% { transform: translate(3px, 2px) rotate(0deg); }
   40% { transform: translate(1px, -1px) rotate(1deg); }
-  50% { transform: translate(-1px, 2px) rotate(-1deg); }
+  50% { transform: translate(-1px, 2px) rotate(-1deg); background-color:#fcd300}
   60% { transform: translate(-3px, 1px) rotate(0deg); }
   70% { transform: translate(3px, 1px) rotate(-1deg); }
   80% { transform: translate(-1px, -1px) rotate(1deg); }
   90% { transform: translate(1px, 2px) rotate(0deg); }
-  100% { transform: translate(1px, -2px) rotate(-1deg); }
+  100% { transform: translate(1px, -2px) rotate(-1deg); background-color:white}
 
 }
 
