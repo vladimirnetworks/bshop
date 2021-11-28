03fb749c8666fe5abdd601aa5671e2df5ebcb37c
vladimirnetworks <vladimirnetworks@gmail.com>
2021-11-28 17:06:33 +0330

    update using fastpush

--- resources/views/main2.blade.php
+++ resources/views/main2.blade.php
@@ -266,10 +266,10 @@ body {
 
   0% { transform: translate(1px, 1px) rotate(0deg);  background-color:white}
   10% { transform: translate(-1px, -2px) rotate(-1deg); }
-  20% { transform: translate(-3px, 0px) rotate(1deg); background-color:#fc0073}
+  20% { transform: translate(-3px, 0px) rotate(1deg); background-color:#00fb59}
   30% { transform: translate(3px, 2px) rotate(0deg); }
   40% { transform: translate(1px, -1px) rotate(1deg); }
-  50% { transform: translate(-1px, 2px) rotate(-1deg); background-color:#fcd300}
+  50% { transform: translate(-1px, 2px) rotate(-1deg); background-color:#00e7ff}
   60% { transform: translate(-3px, 1px) rotate(0deg); }
   70% { transform: translate(3px, 1px) rotate(-1deg); }
   80% { transform: translate(-1px, -1px) rotate(1deg); }
