1dcd60c0c1b8ff04482f13d496f66f2a63ba054a
vladimirnetworks <vladimirnetworks@gmail.com>
2021-11-28 16:38:15 +0330

    update using fastpush

--- resources/views/main2.blade.php
+++ resources/views/main2.blade.php
@@ -263,24 +263,19 @@ body {
 
 
 @keyframes xshake_anim {
-  0% {
-      transform: scale(1.0,1.0);
-      background-color:white;
-  }
 
-
-
-   60% {
-      transform: scale(1.5,1.5);
-      background-color:yellow;
-  }
-
-
-  100% {
-      transform: scale(1,1);
-      background-color:white;
-     
-  }
+  0% { transform: translate(1px, 1px) rotate(0deg); }
+  10% { transform: translate(-1px, -2px) rotate(-1deg); }
+  20% { transform: translate(-3px, 0px) rotate(1deg); }
+  30% { transform: translate(3px, 2px) rotate(0deg); }
+  40% { transform: translate(1px, -1px) rotate(1deg); }
+  50% { transform: translate(-1px, 2px) rotate(-1deg); }
+  60% { transform: translate(-3px, 1px) rotate(0deg); }
+  70% { transform: translate(3px, 1px) rotate(-1deg); }
+  80% { transform: translate(-1px, -1px) rotate(1deg); }
+  90% { transform: translate(1px, 2px) rotate(0deg); }
+  100% { transform: translate(1px, -2px) rotate(-1deg); }
+  
 }
 
 .xshake {
