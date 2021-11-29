531673218bba37264dd1f96117390145596e8193
vladimirnetworks <vladimirnetworks@gmail.com>
2021-11-29 09:49:12 +0330

    update using fastpush

--- resources/views/cartSlider.blade.php
+++ resources/views/cartSlider.blade.php
@@ -1,9 +1,9 @@
-<div style="display:none;opacity:0.5;position:fixed;bottom:0px;left:0px;width:100%;background-color:black;height:100%;z-index:99990" class="cartslider_dim">
+<div style="display:none;opacity:0.5;position:fixed;bottom:0px;left:0px;width:100%;background-color:black;height:100%;z-index:1000" class="cartslider_dim">
 
 </div>
 
 
-<div style="direction:rtl;position:fixed;bottom:0px;left:0px;height:10vh;width:100%;background-color:white;z-index:99991" id="mcartslider" class="text-center cartslider p-3 border-top  p-3 ">
+<div style="direction:rtl;position:fixed;bottom:0px;left:0px;height:10vh;width:100%;background-color:white;z-index:1001" id="mcartslider" class="text-center cartslider p-3 border-top  p-3 ">
  <div class="cartslider_smallview">
  سبد خرید [<span class="cartslider_smallview_text"></span>]
  <button class="btn btn-info m-2 showsabad">
