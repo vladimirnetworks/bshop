d1cde4175306e7c968b5a99ef6d077b6d5b61d62
vladimirnetworks <vladimirnetworks@gmail.com>
2021-11-29 18:55:34 +0330

    update using fastpush

--- resources/views/main2.blade.php
+++ resources/views/main2.blade.php
@@ -489,9 +489,9 @@ xcart.addChangeListener(function() {
 
         var num = $('<div></div>');
 
-          num.append('<button class="btn btn-danger " style="float:left">+</button>');
-          num.append('<span style="float:left" >'+prod.count+'</span>');
-          num.append('<button class="btn btn-danger " style="float:left">-</button>');
+          num.append('<button class="btn btn-danger " style="display:inline-block">+</button>');
+          num.append('<span style="display:inline-block" >'+prod.count+'</span>');
+          num.append('<button class="btn btn-danger " style="display:inline-block">-</button>');
         
         cont.append($('<div style="display:inline-block"></div>').append(num));
 
