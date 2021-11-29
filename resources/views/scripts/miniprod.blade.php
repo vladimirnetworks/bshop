ca5514dd813ab5e9930a481fc3ce4e6b503b6ba7
vladimirnetworks <vladimirnetworks@gmail.com>
2021-11-29 13:19:37 +0330

    update using fastpush

--- resources/views/scripts/miniprod.blade.php
+++ resources/views/scripts/miniprod.blade.php
@@ -119,7 +119,7 @@ photos.append($(mySwipe.HTMLElement));
 
 
 var title = $('<div class="p-1 text-dark" style="font-size:120;font-weight:bold">'+vals.title+'</div>');
-var price = $('<div class="p-1 text-success" style="font-size:150%;font-weight:bold">'+(vals.price)+' تومان </div>');
+var price = $('<div class="p-1 text-success" style="font-size:150%;font-weight:bold">'+farsi_price(vals.price)+' تومان </div>');
 var caption = $('<ul style="text-align: right;font-size:90%">'+vals.caption+'</ul>');
 
 
