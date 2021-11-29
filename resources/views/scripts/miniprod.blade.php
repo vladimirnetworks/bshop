63500ef6b5eee6fbad1f845e042cc296aab6c838
vladimirnetworks <vladimirnetworks@gmail.com>
2021-11-29 12:18:27 +0330

    update using fastpush

--- resources/views/scripts/miniprod.blade.php
+++ resources/views/scripts/miniprod.blade.php
@@ -196,7 +196,7 @@ fly.css({bottom:"-100%",width:"1vw"});
 
  },700);
 
-
+alert(parseInt(vals.price));
 addtocart({id:vals.id,title:vals.title,tinytitle:vals.tinytitle,price:parseInt(vals.price)});
 alert("add");
 
