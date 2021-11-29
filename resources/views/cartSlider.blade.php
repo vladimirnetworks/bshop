c2e32cb274935e980155ba549592ecffda962f17
vladimirnetworks <vladimirnetworks@gmail.com>
2021-11-29 13:48:40 +0330

    update using fastpush

--- resources/views/cartSlider.blade.php
+++ resources/views/cartSlider.blade.php
@@ -3,7 +3,7 @@
 </div>
 
 
-<div style="justify-content: center;display: flex;align-items: center;direction:rtl;position:fixed;bottom:0px;left:0px;width:100%;background-color:white;z-index:1001" id="mcartslider" class="text-left cartslider border-top ">
+<div style="padding:1vh;justify-content: center;display: flex;align-items: center;direction:rtl;position:fixed;bottom:0px;left:0px;width:100%;background-color:white;z-index:1001" id="mcartslider" class="text-left cartslider border-top ">
  <div class="cartslider_smallview">
 
 <span class="cartslider_smallview_text" style="font-size:90%;"></span>
