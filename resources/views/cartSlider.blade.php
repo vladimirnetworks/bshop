e632037fe85eeb79d24853eaabe761b94e11ab1a
vladimirnetworks <vladimirnetworks@gmail.com>
2021-11-30 18:44:50 +0330

    update using fastpush

--- resources/views/cartSlider.blade.php
+++ resources/views/cartSlider.blade.php
@@ -1,55 +1,66 @@
-<div style="display:none;opacity:0.5;position:fixed;bottom:0px;left:0px;width:100%;background-color:black;height:100%;z-index:1000" class="cartslider_dim">
+<div
+   style="display:none;opacity:0.5;position:fixed;bottom:0px;left:0px;width:100%;background-color:black;height:100%;z-index:1000"
+   class="cartslider_dim">
 
 </div>
 
+<div
+   style="box-shadow:rgb(136 136 136) 0px 0px 10px;transition:height 0.3s;height:9vh;direction:rtl;position:fixed;bottom:-9vh;left:0px;width:100%;background-color:white;z-index:1001"
+   id="mcartslider" class="text-left cartslider border-top ">
+   <div class="cartslider_smallview"
+      style="transition:all 0.3s;display:flex;height:100%;justify-content: center;align-items: center">
 
-<div style="box-shadow:rgb(136 136 136) 0px 0px 10px;transition:height 0.3s;height:9vh;direction:rtl;position:fixed;bottom:-9vh;left:0px;width:100%;background-color:white;z-index:1001" id="mcartslider" class="text-left cartslider border-top ">
- <div class="cartslider_smallview" style="transition:all 0.3s;display:flex;height:100%;justify-content: center;align-items: center">
+      <span class="cartslider_smallview_text" style="font-size:90%;"></span>
 
-<span class="cartslider_smallview_text" style="font-size:90%;"></span>
+      <button style="font-size:90%;" class="btn btn-info  showsabad">
 
- <button style="font-size:90%;" class="btn btn-info  showsabad">
+         سبد خرید
 
-سبد خرید
+      </button>
+   </div>
 
- </button>
- </div>
+   <div class="cartslider_bigview text-center" style="display:none">
 
- <div class="cartslider_bigview text-center" style="display:none">
- 
- <div class="cartslider_bigview_cart"></div>
- 
-<div class="w-100 text-center p-2 m-2">
-  <button class="finishshop btn btn-primary">finish</button>
+      <div class="cartslider_bigview_cart"></div>
+
+      <div class="w-100 text-center p-2 m-2">
+         <button class="finishshop btn btn-primary">finish</button>
+      </div>
+
+   </div>
+
 </div>
-
- </div>
-
-</div>
-
 
 <script>
-
-$(".showsabad").click(function() {
-   cartup(); 
-   hpu("cartup");
-});
-
-$(".cartslider_dim").click(function() {
-   cartdown(); 
-});
-
-$(".finishshop").click(function() {
-   cartdown(); 
-   $("#getNumberModal").modal("show");
-    hpu("finishcart");
-
-    toyou("preorder",xcart.items(),function(res) {
-       console.log(res);
-       myorder.orderid = res.data.id;
-       myorder.shipping = res.data.shipping;
-    });
-
-});
-
+   $(".showsabad").click(function() {
+      cartup();
+      hpu("cartup");
+   });
+   $(".cartslider_dim").click(function() {
+      cartdown();
+   });
+   $(".finishshop").click(function() {
+            cartdown();
+            $("#getNumberModal").modal("show");
+            hpu("finishcart");
+            toyou("preorder", xcart.items(), function(res) {
+               console.log(res);
+               myorder.orderid = res.data.id;
+               myorder.shipping = res.data.shipping;
+            });
+            $("#shippingx").empty();
+            for (var i = 0; i < myorder.shipping.length; i++) {
+               var maindiv = $("<div></div>");
+               var labelx = $('<label></label>');
+               if (i === 0) {
+                  var inputx = $('<input checked type="radio" class="" name="shiptype" value="' + i + '">');
+               } else {
+                  var inputx = $('<input type="radio" class="" name="shiptype" value="' + i + '">');
+               }
+               var textx = $('<span>' + myorder.shipping[i].text + '</span>');
+               labelx.append(inputx);
+               labelx.append(textx);
+               maindiv.append(labelx);
+               $("#shippingx").append(maindiv);
+            });
 </script>
--- resources/views/orderModals/getAddress.blade.php
+++ resources/views/orderModals/getAddress.blade.php
@@ -3,83 +3,52 @@
       <div class="modal-content">
          <!-- Modal Header -->
          <div class="modal-header">
-       
+
             <button type="button" class="close" data-dismiss="modal">&times;</button>
          </div>
          <!-- Modal body -->
          <div class="modal-body text-center">
-            <div class="enternumber" >
-آدرستو رو وارد کنید
+            <div class="enternumber">
+               آدرستو رو وارد کنید
                <br>
                <div>
-                  <input type="hidden" id="myModal_next" value="address"/> 
-                  <form id="reggaddress" name="reggaddressform"  action="/" method="post">
-                     <input readonly class="ordernumber" style="font-size:24px;" type="hidden"  placeholder="شماره سفارش"> 
+                  <input type="hidden" id="myModal_next" value="address" />
+                  <form id="reggaddress" name="reggaddressform" action="/" method="post">
+                     <input readonly class="ordernumber" style="font-size:24px;" type="hidden"
+                        placeholder="شماره سفارش">
                      <div class="row p-3">
-                        <input class="form-control col-12" style="font-size:24px;text-align:right;direction:rtl" type="text" id="getaddress" placeholder="آدرس"> 
+                        <input class="form-control col-12" style="font-size:24px;text-align:right;direction:rtl"
+                           type="text" id="getaddress" placeholder="آدرس">
                         <button type="submit" class="btn btn-success col-2">ثبت</button>
                      </div>
 
 
-<div id="shippingx"></div>
+                     <div id="shippingx"></div>
 
 
                   </form>
                </div>
             </div>
-            <div class="waitinnumber" style="display:none">   
-              
+            <div class="waitinnumber" style="display:none">
 
-              
+
+
             </div>
          </div>
          <!-- Modal footer -->
       </div>
    </div>
 </div>
-
 <script>
-$( "#getAddressModal" ).on('shown.bs.modal', function(){
+   $("#getAddressModal").on('shown.bs.modal', function() {
+      $("#getaddress").trigger('focus');
 
- $("#getaddress").trigger('focus');
-
-$("#shippingx").empty();
-for (var i=0;i<myorder.shipping.length;i++) {
-
-var maindiv = $("<div></div>");
-var labelx = $('<label></label>');
-if (i === 0 ) {
-var inputx = $('<input checked type="radio" class="" name="shiptype" value="'+i+'">');
-
-} else {
-var inputx = $('<input type="radio" class="" name="shiptype" value="'+i+'">');
-}
-var textx = $('<span>'+myorder.shipping[i].text+'</span>');
-
-labelx.append(inputx);
-labelx.append(textx);
-maindiv.append(labelx);
-
-$("#shippingx").append(maindiv);
-
-
-}
-
-
-
-
-});
-
-
-$("#reggaddress").on('submit',function () {
-
-    $( "#getAddressModal" ).modal("hide");
-  
-    $( "#successOrderModal" ).modal("show");
-
-    hpu("addedaddress");
-
-    
-    return false;
-});
+      }
+   });
+   $("#reggaddress").on('submit', function() {
+      $("#getAddressModal").modal("hide");
+      $("#successOrderModal").modal("show");
+      hpu("addedaddress");
+      return false;
+   });
 </script>
