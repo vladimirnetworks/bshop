18adf029585093e6d42f77276c52aee43eb4b6b6
vladimirnetworks <vladimirnetworks@gmail.com>
2021-11-30 11:23:53 +0330

    update using fastpush

--- app/Http/Controllers/ProductController.php
+++ app/Http/Controllers/ProductController.php
@@ -16,22 +16,34 @@ class ProductController extends Controller
     public function indexxv(Request $request)
     {
 
+
         $prods = Product::orderBy('id', 'DESC')->paginate(10, ['*'], 'page', $request->page);
 
 
         $prods->each(function ($item) {
 
-          
             $phot = json_decode($item->photos, true);
-               
-           
+
 
             if (isset($phot[0])) {
                 $item->photo = $phot[0]['medium'];
             }
 
 
+            $item->jsondata = str_replace('','',json_encode([
+
+                "id"=>$item->id,
+                "title"=>$item->title,
+                "tinytitle"=>$item->tinytitle,
+                "price"=>$item->price,
+                "caption"=>lize($item->caption),
+                "photos"=>$phot,
+                
+
+            ]));
+
         });
+
 
    
 
