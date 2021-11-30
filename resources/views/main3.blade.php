6d96d848a80fb121391c8b4546326db2aac56b0a
vladimirnetworks <vladimirnetworks@gmail.com>
2021-11-30 12:22:22 +0330

    update using fastpush

--- resources/views/main3.blade.php
+++ resources/views/main3.blade.php
@@ -119,16 +119,16 @@ $('.loader').empty();
 
 apix = new api();
 
-apix.get("index",function(item) {
+apix.get("index",function(vals) {
 
     var xx = '<div style="transition: all .150s" class="rounded col-4 col-sm-3  p-2 text-center miniproduct" data-me=""> \
      <div class=" h-100 " style="direction:rtl;flex-direction:column;display:flex"> \
-      <span>  <img class="mw-100" src="'+item.photo+'"></span> \
+      <span>  <img class="mw-100" src="'+vals.photo+'"></span> \
       <div style="margin-top:auto"> \
-      <span style="color:#535353" href="product/47" class="d-block">'+item.tinytitle+'</span> \
+      <span style="color:#535353" href="product/47" class="d-block">'+vals.tinytitle+'</span> \
       </div> \
       <div style="margin-top:auto"> \
-      <span style="color:#232933">'+item.price+'</span><span style="font-size:.714rem ; color:#232933">تومان \
+      <span style="color:#232933">'+vals.price+'</span><span style="font-size:.714rem ; color:#232933">تومان \
       </span> \
       </div> \
    </div> \
@@ -137,10 +137,6 @@ apix.get("index",function(item) {
 
 var jprod = $(xx);
 
-jprod.click(function() {
-         $('.bigprod').empty();
-         $('.bigprod').append($('<div>'+item.title+'</div>'));
-});
 
 
 jprod.on("touchstart click",function() {
@@ -152,6 +148,166 @@ jprod.on("touchstart click",function() {
 
 
 });
+
+
+/**/
+
+
+jprod.on("click",function(e) {
+
+
+
+
+
+
+
+
+var photoitems = [];
+console.log(vals.photos);
+for (var i=0;i<vals.photos.length;i++) {
+   photoitems.push('<div class="myitem"><img style="width:100%" src="'+vals.photos[i].medium+'"  /></div>');
+}
+
+var mySwipe = new SwiperBox({
+					items:photoitems					
+					});
+
+
+
+var cont = $('<div class="saving p-1 m-1 rounded " style="min-height:70vh;background-color:white;direction:rtl"></div>');
+
+var photos = $('<div style="width:60vw;margin-right: auto;margin-left: auto;max-width:300px;"></div>');
+photos.append($(mySwipe.HTMLElement));
+
+
+var title = $('<div class="p-1 text-dark" style="font-size:120;font-weight:bold">'+vals.title+'</div>');
+var price = $('<div class="p-1 text-success" style="font-size:150%;font-weight:bold">'+farsi_price(vals.price)+' تومان </div>');
+var caption = $('<ul style="text-align: right;font-size:90%">'+vals.caption+'</ul>');
+
+
+cont.append(photos);
+cont.append(title);
+cont.append(price);
+cont.append(caption);
+
+
+
+
+
+var kharid = $('<button class="btn btn-danger btn-lg m-2">خرید</button>');
+
+kharid.click(function(e) {
+
+
+
+var fly = $('<div class="fly" style="position:fixed;bottom:-100%;left:-100%;background-color:white;z-index:99991"></div>');
+$('.fly').remove();
+var cloned_photos = photos.clone();
+
+cloned_photos.css({width:"100%"});
+
+var pattern = /jpg/;
+var flyingimage = "";
+for (var i=0;i<photoitems.length;i++) {
+
+  if (pattern.test(photoitems[i].innerHTML)) {
+    flyingimage = photoitems[i].innerHTML;
+    break;
+  }
+}
+
+fly.append($(flyingimage));
+
+//fly.append(title.clone());
+
+
+
+var position_from_top = photos.offset().top - $(window).scrollTop();
+
+xofsset = photos.offset();
+
+
+  var btm = $(window).height()-(position_from_top+photos.height());
+
+fly.css({
+  width:photos.width()+"px",
+bottom:btm+"px",
+height:photos.height()+"px",
+left:xofsset.left+"px",
+});
+
+$('body').append(fly);
+
+
+//$('#tr').get(0).play();
+
+
+
+fly.css({transition:'all 1.5s'});
+
+
+
+    $(".cartslider").css({
+        "height": "30vh"
+    });
+
+        $(".cartslider_smallview").css({
+        "align-items": "start"
+    });
+
+
+
+
+
+ setTimeout(function() {
+fly.css({bottom:"-100%",width:"1vw"});
+},200);
+
+
+ $('.cartslider').removeClass("xshake");
+
+  setTimeout(function() {
+
+ $('.cartslider').addClass("xshake");
+ //$('#shopp').get(0).play();
+
+ },700);
+
+
+  setTimeout(function() {
+
+     $(".cartslider").css({
+        "height": "9vh"
+     });
+
+
+        $(".cartslider_smallview").css({
+        "align-items": "center"
+    });
+
+
+ },900);
+
+
+
+
+
+
+addtocart({id:vals.id,title:vals.title,tinytitle:vals.tinytitle,price:parseInt(vals.price)});
+
+
+});
+
+cont.append(kharid);
+
+$('.bigprod').append(cont);
+
+
+
+
+});
+/**/
+
 
 $('.loader').append(jprod);
 
