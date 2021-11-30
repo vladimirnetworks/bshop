3c6d93e5d72ab3ef656d19a8b89332128ddbf9da
vladimirnetworks <vladimirnetworks@gmail.com>
2021-11-30 12:25:35 +0330

    update using fastpush

--- resources/views/main3.blade.php
+++ resources/views/main3.blade.php
@@ -163,9 +163,10 @@ jprod.on("click",function(e) {
 
 
 var photoitems = [];
-console.log(vals.photos);
+
 for (var i=0;i<vals.photos.length;i++) {
    photoitems.push('<div class="myitem"><img style="width:100%" src="'+vals.photos[i].medium+'"  /></div>');
+   console.log(vals.photos[i].medium);
 }
 
 var mySwipe = new SwiperBox({
