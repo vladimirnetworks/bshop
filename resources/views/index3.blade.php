8b636901b9a99cd13837be793096341bb341d4ba
vladimirnetworks <vladimirnetworks@gmail.com>
2021-11-30 12:29:09 +0330

    update using fastpush

--- resources/views/index3.blade.php
+++ resources/views/index3.blade.php
@@ -2,7 +2,7 @@
 
 @section('main')
 
-<div class="bigprod"></div>
+<div class="bigprod" style="text-align:center"></div>
 
 <div class="loader row justify-content-center" style="margin-left:0px;margin-right:0px"></div>
 
