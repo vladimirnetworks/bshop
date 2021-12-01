c1e9b7c55095cd7adebaa8d4f03cb133d9fffb45
vladimirnetworks <vladimirnetworks@gmail.com>
2021-12-01 09:27:24 +0330

    update using fastpush

--- app/Http/Controllers/Paymentvarify.php
+++ app/Http/Controllers/Paymentvarify.php
@@ -28,7 +28,7 @@ class Paymentvarify extends Controller
         $data = array("merchant_id" => "14b79a43-cb9b-44eb-b4d0-e8b37343278d", "authority" => $Authority, "amount" => $amount*10);
         
  
-        print_r($data);
+        //print_r($data);
 
         $jsonData = json_encode($data);
         $ch = curl_init('https://api.zarinpal.com/pg/v4/payment/verify.json');
