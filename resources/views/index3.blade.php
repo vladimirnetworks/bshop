4200ec34d5075564747702f1910ebf2e39c5ab92
vladimirnetworks <vladimirnetworks@gmail.com>
2021-12-02 20:39:39 +0330

    update using fastpush

--- resources/views/index3.blade.php
+++ resources/views/index3.blade.php
@@ -4,7 +4,8 @@
 
 <div class="bigprod" style="text-align:center"></div>
 
-<div class="catmain text-center" style=""></div>
+<div class="catmain text-center" style="white-space: nowrap;overflow: auto;direction: rtl;
+"></div>
 
 <div class="loader row justify-content-center" style="margin-left:0px;margin-right:0px"></div>
 
