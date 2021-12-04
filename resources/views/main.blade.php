05f286ee17cbe200845db068dc16677b6a454346
vladimirnetworks <vladimirnetworks@gmail.com>
2021-12-04 19:32:40 +0330

    update using fastpush

--- resources/views/main.blade.php
+++ resources/views/main.blade.php
@@ -39,7 +39,7 @@
 
   <div style="position:fixed;height:7vh;background-color:white;top:0px;left:0px;text-align:center;width:100%" >
    <form>
-     <input type="text" class="form-control m-1" style="direction:rtl" placeholder="جستجو در محصولات">
+     <input type="text" class="form-control mt-1" style="direction:rtl;widht:90%" placeholder="جستجو در محصولات">
    </form>
   </div>
 
