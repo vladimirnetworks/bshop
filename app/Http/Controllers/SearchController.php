aa4b8869a9c4b494ca3636c9903f1bd8637feb66
vladimirnetworks <vladimirnetworks@gmail.com>
2021-12-05 09:14:44 +0330

    update using fastpush

--- app/Http/Controllers/SearchController.php
+++ app/Http/Controllers/SearchController.php
@@ -10,35 +10,44 @@ class SearchController extends Controller
     public function search(Request $request)
     {
 
-      
-       // $prods = Product::orderBy('id', 'DESC')->paginate(10, ['*'], 'page', $request->page);
+
+        // $prods = Product::orderBy('id', 'DESC')->paginate(10, ['*'], 'page', $request->page);
 
 
         $searchTerms = explode(' ', $request->q);
         $query = Product::query();
-        
 
 
-        $query->where('searchkey','like','%'.$searchTerms[0].'%');
-        foreach($searchTerms as $k=>$searchTerm){
-          
-            if ($k>0) {
-             $query->orWhere('searchkey','like','%'.$searchTerms[0].'%');
+
+        $query->where('searchkey', 'like', '%' . $searchTerms[0] . '%');
+        foreach ($searchTerms as $k => $searchTerm) {
+
+            if ($k > 0) {
+                $query->orWhere('searchkey', 'like', '%' . $searchTerms[0] . '%');
             }
-
         }
 
-        
+
         $results = $query->get();
 
 
-        return  $results;
 
-       /* return ["data"=>
+        if ($results->count) {
 
-            ["notfound"=>true]
+            return [
+                "data" =>
 
-        ];
-        */
+                $results
+
+            ];
+        } else {
+
+            return [
+                "data" =>
+
+                ["notfound" => true]
+
+            ];
+        }
     }
 }
