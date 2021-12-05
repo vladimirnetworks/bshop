fabfb7c0eb65a044a9aca78549c870d1fd8319a6
vladimirnetworks <vladimirnetworks@gmail.com>
2021-12-05 07:04:13 +0330

    update using fastpush

--- app/Http/Controllers/SearchController.php
+++ app/Http/Controllers/SearchController.php
@@ -14,33 +14,6 @@ class SearchController extends Controller
         $prods = Product::orderBy('id', 'DESC')->paginate(10, ['*'], 'page', $request->page);
 
 
-        $prods->each(function ($item) {
-
-            $phot = json_decode($item->photos, true);
-
-
-            if (isset($phot[0])) {
-                $item->photo = $phot[0]['medium'];
-            }
-
-
-            $item->jsondata = str_replace('','',json_encode([
-
-                "id"=>$item->id,
-                "title"=>$item->title,
-                "tinytitle"=>$item->tinytitle,
-                "price"=>$item->price,
-                "caption"=>lize($item->caption),
-                "photos"=>$phot,
-                
-
-            ]));
-
-        });
-
-
-   
-
 
         return  $prods;
 
