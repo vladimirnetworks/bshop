0cb92f32c53f4a8c83c5b2ca9c2e5f9a59ff72d1
vladimirnetworks <vladimirnetworks@gmail.com>
2021-12-05 05:43:16 +0330

    update using fastpush

--- resources/views/main.blade.php
+++ resources/views/main.blade.php
@@ -42,10 +42,11 @@
    height: 100%;
    justify-content: center;
    align-items: center;position:relative" class="px-2">
-     <input autocomplete="off" id="seachbox" type="text" class="form-control " style="direction:rtl;display:inline-block" placeholder="جستجو در محصولات">
-  
-     <div style="z-index:99999;position:fixed;background-color:white;bottom:0px;height:100vh"></div>
 
+     <input autocomplete="off" id="seachbox" type="text" class="form-control " style="direction:rtl;display:inline-block" placeholder="جستجو در محصولات">
+     
+     <div style="width:100%;height:5px"></div>
+      <div style="display:none;z-index:99999;position:fixed;background-color:white;bottom:0px;height:100vh"></div>
     </form>
   </div>
 
