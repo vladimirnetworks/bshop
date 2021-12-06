1f1035c19ec6f9ba845239c4d1eed3e31c009e6c
vladimirnetworks <vladimirnetworks@gmail.com>
2021-12-06 06:56:24 +0330

    update using fastpush

--- public/scripts/bshDom.js
+++ public/scripts/bshDom.js
@@ -188,11 +188,11 @@ function openprod(vals, noanim = null) {
 
 
 
-    var added = $('<div class="border border-pirmary"></div>');
+    var added = $('<div style="display: flex;flex-direction: column"></div>');
     var count_added = $('<span></span>');
 
-    var bez = $('<button style="display:inline-block;border-radius: 0;" class="btn btn-danger rounded-right btn-sm" >+</button>');
-    var men = $('<button style="display:inline-block;border-radius: 0;" class="btn btn-danger rounded-left btn-sm" >-</button>');
+    var bez = $('<button style="display:inline-block;border-radius: 0;" class="btn btn-danger rounded-top btn-sm" >+</button>');
+    var men = $('<button style="display:inline-block;border-radius: 0;" class="btn btn-danger rounded-bottom btn-sm" >-</button>');
 
     added.append(bez);
     added.append(count_added);
