1e75375072252a50d966792465af0c25dcc6f185
vladimirnetworks <vladimirnetworks@gmail.com>
2021-12-06 06:09:29 +0330

    update using fastpush

--- resources/views/main.blade.php
+++ resources/views/main.blade.php
@@ -60,11 +60,11 @@
   </div>
 
 
-   <div  style="z-index:999;position:absolute;top: 3%;left: 6%;width: 9vw; display:none;opacity:0.8;max-width: 51px;" class="prodtopbar" id="mag" >
+   <div  style="z-index:999;position:absolute;top: 3%;left: 6%;width: 9vw; display:none;opacity:0.8;max-width: 51px;" class="TransFormAnim prodtopbar" id="mag" >
         <img src="/icons/mag.png" style="width: 100%" />
    </div>
 
-   <div  style="z-index:999;position:absolute;top: 3%;right: 6%;width: 9vw;display:none;opacity:0.8;max-width: 51px;" class="prodtopbar"  id="menu" >
+   <div  style="z-index:999;position:absolute;top: 3%;right: 6%;width: 9vw;display:none;opacity:0.8;max-width: 51px;" class="TransFormAnim prodtopbar"  id="menu" >
     <img src="/icons/menu.png" style="width: 100%" />
    </div>   
 
