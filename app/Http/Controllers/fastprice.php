2576409e657af3d1dd05bd15689f4dedfa4d0156
vladimirnetworks <vladimirnetworks@gmail.com>
2021-12-07 19:01:06 +0330

    update using fastpush

--- app/Http/Controllers/fastprice.php
+++ app/Http/Controllers/fastprice.php
@@ -61,7 +61,7 @@ class fastprice extends Controller
         //header('Access-Control-Allow-Methods: *');
         //header('Access-Control-Allow-Headers: *');
 
-
+         dd($Product);
         $Product->price = $request->price;
 
 
