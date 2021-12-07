0bd2148d3a0740154e152e1f23fbaf41339bcbe6
vladimirnetworks <vladimirnetworks@gmail.com>
2021-12-07 06:30:04 +0330

    update using fastpush

--- app/Http/Middleware/benmiddleware.php
+++ app/Http/Middleware/benmiddleware.php
@@ -20,41 +20,43 @@ class benmiddleware
     {
 
 
-       
-       
+
+
 
         monitor::create([
-            "url"=>$_SERVER['REQUEST_URI'],
-            "useragent"=>$_SERVER['HTTP_USER_AGENT'],
-            "cookie"=>(isset($_COOKIE) ? json_encode($_COOKIE) : null),
-            "get_param"=>(isset($_GET) ? json_encode($_GET) : null),
-            "post_param"=>(isset($_POST) ? json_encode($_POST) : null),
-            "phpinput"=>file_get_contents('php://input'),
-            "referer"=>(isset($_SERVER['HTTP_REFERER']) ? $_SERVER['HTTP_REFERER'] : null),
-            "ip"=>$_SERVER['REMOTE_ADDR']
+            "url" => $_SERVER['REQUEST_URI'],
+            "useragent" => $_SERVER['HTTP_USER_AGENT'],
+            "cookie" => (isset($_COOKIE) ? json_encode($_COOKIE) : null),
+            "get_param" => (isset($_GET) ? json_encode($_GET) : null),
+            "post_param" => (isset($_POST) ? json_encode($_POST) : null),
+            "phpinput" => file_get_contents('php://input'),
+            "referer" => (isset($_SERVER['HTTP_REFERER']) ? $_SERVER['HTTP_REFERER'] : null),
+            "ip" => $_SERVER['REMOTE_ADDR']
         ]);
-        
+
         return $next($request);
     }
 
 
     public function terminate($request, $response)
-	{
+    {
 
-        
-        
-        monitor::create([
-            "url"=>$_SERVER['REQUEST_URI'],
-            "useragent"=>$_SERVER['HTTP_USER_AGENT'],
-            "cookie"=>(isset($_COOKIE) ? json_encode($_COOKIE) : null),
-            "get_param"=>(isset($_GET) ? json_encode($_GET) : null),
-            "post_param"=>(isset($_POST) ? json_encode($_POST) : null),
-            "phpinput"=>file_get_contents('php://input'),
-            "referer"=>(isset($_SERVER['HTTP_REFERER']) ? $_SERVER['HTTP_REFERER'] : null),
-            "ip"=>$_SERVER['REMOTE_ADDR'],
-            "terminate_response"=>$response
-        ]);
 
-	}
+        if (preg_match("!500 Internal Server Error!", $response) || preg_match("!404 Not Found!", $response)) {
 
+
+
+            monitor::create([
+                "url" => $_SERVER['REQUEST_URI'],
+                "useragent" => $_SERVER['HTTP_USER_AGENT'],
+                "cookie" => (isset($_COOKIE) ? json_encode($_COOKIE) : null),
+                "get_param" => (isset($_GET) ? json_encode($_GET) : null),
+                "post_param" => (isset($_POST) ? json_encode($_POST) : null),
+                "phpinput" => file_get_contents('php://input'),
+                "referer" => (isset($_SERVER['HTTP_REFERER']) ? $_SERVER['HTTP_REFERER'] : null),
+                "ip" => $_SERVER['REMOTE_ADDR'],
+                "terminate_response" => $response
+            ]);
+        }
+    }
 }
