2f5575bd4fd2665d25632075968cb5a108bbcaf7
vladimirnetworks <vladimirnetworks@gmail.com>
2021-12-08 10:50:23 +0330

    update using fastpush

--- public/scripts/bsh.js
+++ public/scripts/bsh.js
@@ -332,7 +332,7 @@ xcart.addChangeListener(function() {
 
     var tot2 = xcart.total();
     if (tot2.count > 0) {
-        $(".cartslider_bigview_cart_total").html('<div>مچموع : ' + farsi_price(tot2.amount) + '</div>');
+        $(".cartslider_bigview_cart_total").html('<div>مجموع : ' + farsi_price(tot2.amount) + '</div>');
 
     } else {
         $(".cartslider_bigview_cart_total").html('');
