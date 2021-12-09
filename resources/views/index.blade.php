dac606983715c0e8b7725bb9db70cb4d24b7ce04
vladimirnetworks <vladimirnetworks@gmail.com>
2021-12-09 10:12:35 +0330

    update using fastpush

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -5,7 +5,7 @@
 <div class="mjax">
 <div class="bigprod" style="text-align:center"></div>
 
-<div class="catmain text-center p-1 m-1" style="white-space: nowrap;overflow: auto;direction:rtl;"></div>
+<div class="catmain text-center p-1 m-1" style="display:none;white-space: nowrap;overflow: auto;direction:rtl;"></div>
 
 <div class="loader row justify-content-center" style="margin-left:0px;margin-right:0px"></div>
 
