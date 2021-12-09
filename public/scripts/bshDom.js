6eb87858c62269f5f0b910147e8e96a28963866e
vladimirnetworks <vladimirnetworks@gmail.com>
2021-12-09 10:27:22 +0330

    update using fastpush

--- public/scripts/bshDom.js
+++ public/scripts/bshDom.js
@@ -295,7 +295,7 @@ function loadtoloader(target, path) {
             }
         });
 
-        var xx = '<div style="transition: all .150s" class="rounded col-4 col-sm-3  pb-2 px-2 text-center miniproduct" data-me=""> \
+        var xx = '<div style="transform: scaleX(1.5);transition: all .150s" class="rounded col-4 col-sm-3  pb-2 px-2 text-center miniproduct" data-me=""> \
 <div class=" h-100 " style="direction:rtl;flex-direction:column;display:flex"> \
 <span>  <img class="mw-100" src="/' + vals.photo + '"></span> \
 <div class="incart" style="width:30%;position: absolute;right: 10%;top: 10%;"></div>\
@@ -357,6 +357,9 @@ function loadtoloader(target, path) {
 
         $(target).append(jprod);
 
+        setTimeout(function() {
+            jprod.css({ "transform": "scaleX(1.0);" });
+        }, 100);
 
 
 
