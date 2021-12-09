4c251ad41ab0642874139e00b9626b29b67116c0
vladimirnetworks <vladimirnetworks@gmail.com>
2021-12-09 10:17:16 +0330

    update using fastpush

--- public/scripts/bshDom.js
+++ public/scripts/bshDom.js
@@ -267,7 +267,7 @@ function openprod(vals, noanim = null) {
     $('.bigprod').empty();
     $('.bigprod').append(cont);
     hidetop();
-    $('.bigprod').append($('<hr style="margin:0px;padding:0px">'));
+    $('.bigprod').append($('<hr style="margin-top:2px;padding:0px">'));
 
 
 }
