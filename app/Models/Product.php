5974659aa2a2c6955c40340e53339c683b963c88
vladimirnetworks <vladimirnetworks@gmail.com>
2021-12-09 10:59:29 +0330

    update using fastpush

--- app/Models/Product.php
+++ app/Models/Product.php
@@ -81,7 +81,7 @@ class Product extends Model
 
       //  return 'loadcat(".catmain","catload",{"type":"relateto","id":"'.$this->id.'"});';
 
-        return 'loadtoloader(".loader","relateto/'.$this->id.'");';
+        return 'setTimeout(function() {loadtoloader(".loader","relateto/'.$this->id.'");},1000);';
 
     }
 
