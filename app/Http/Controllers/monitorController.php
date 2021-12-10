be7f994b7a275cf53fd03417cbfc2b744390e6d8
vladimirnetworks <vladimirnetworks@gmail.com>
2021-12-10 18:52:50 +0330

    update using fastpush

--- app/Http/Controllers/monitorController.php
+++ app/Http/Controllers/monitorController.php
@@ -4,15 +4,18 @@ namespace App\Http\Controllers;
 
 use App\Models\monitor;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
 
 class monitorController extends Controller
 {
      public function LatestUsers(Request $request)
      {
        
-      $latest = monitor::where("liteauth_id",">","0")->orderBy('liteauth_id', 'DESC')->paginate(20, ['*'], 'page', $request->page);
+      //$latest = monitor::where("liteauth_id",">","0")->orderBy('liteauth_id', 'DESC')->paginate(20, ['*'], 'page', $request->page);
 
-return  $latest ;
+      $latest = DB::select("SELECT * FROM monitor");
+
+       return  $latest ;
 
      }
 }
