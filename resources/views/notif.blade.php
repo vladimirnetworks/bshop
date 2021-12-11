43899dc5ea131928f98462806b556beca599dd33
vladimirnetworks <vladimirnetworks@gmail.com>
2021-12-11 14:16:51 +0330

    update using fastpush

--- resources/views/notif.blade.php
+++ resources/views/notif.blade.php
@@ -77,6 +77,24 @@ if ('serviceWorker' in navigator && 'PushManager' in window) {
   pushButton.textContent = 'Push Not Supported';
 }
 
+
+
+
+function initializeUI() {
+  // Set the initial subscription value
+  swRegistration.pushManager.getSubscription()
+  .then(function(subscription) {
+    isSubscribed = !(subscription === null);
+    if (isSubscribed) {
+      console.log('User IS subscribed.');
+    } else {
+      console.log('User is NOT subscribed.');
+    }
+    updateBtn();
+  });
+}
+
+
 </script>
 
 </body>
