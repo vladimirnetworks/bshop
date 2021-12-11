08e77975d7b32bdc52bd8f5e494a24be9d1830ff
vladimirnetworks <vladimirnetworks@gmail.com>
2021-12-11 13:54:33 +0330

    update using fastpush

--- resources/views/notif.blade.php
+++ resources/views/notif.blade.php
@@ -25,10 +25,10 @@ if ('serviceWorker' in navigator) {
   window.addEventListener('load', function() {
     navigator.serviceWorker.register('/scripts/sw.js').then(function(registration) {
       // Registration was successful
-      console.log('ServiceWorker registration successful with scope: ', registration.scope);
+      alert('ServiceWorker registration successful with scope: ', registration.scope);
     }, function(err) {
       // registration failed :(
-      console.log('ServiceWorker registration failed: ', err);
+      alert('ServiceWorker registration failed: ', err);
     });
   });
 }
