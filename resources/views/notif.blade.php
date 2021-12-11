514a337c815a0bbf5da8f28c2ad64fbf6ff79c45
vladimirnetworks <vladimirnetworks@gmail.com>
2021-12-11 14:12:19 +0330

    update using fastpush

--- resources/views/notif.blade.php
+++ resources/views/notif.blade.php
@@ -18,31 +18,20 @@
 <script>
 
 
-
-
-
-if ('serviceWorker' in navigator) {
-  window.addEventListener('load', function() {
-    navigator.serviceWorker.register('/sw.js').then(function(registration) {
-      // Registration was successful
-      console.log(registration);
-
-      registration.pushManager.subscribe({
-
-        userVisibleOnly : true
-
-      }).then(function(sub) {
-        console.log(sub);
-      });
-
-     // alert('ServiceWorker registration successful with scope: ', registration.scope);
-    }, function(err) {
-      // registration failed :(
-      //alert('ServiceWorker registration failed: ', err);
-    });
+if ('serviceWorker' in navigator && 'PushManager' in window) {
+  console.log('Service Worker and Push is supported');
+  navigator.serviceWorker.register('sw.js')
+  .then(function(swReg) {
+    console.log('Service Worker is registered', swReg);
+    swRegistration = swReg;
+  })
+  .catch(function(error) {
+    console.error('Service Worker Error', error);
   });
+} else {
+  console.warn('Push messaging is not supported');
+  pushButton.textContent = 'Push Not Supported';
 }
-
 
 </script>
 
