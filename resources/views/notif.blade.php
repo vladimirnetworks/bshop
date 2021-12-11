a599c6843961ee76e7bc14f819a929a8e0b21e1e
vladimirnetworks <vladimirnetworks@gmail.com>
2021-12-11 14:13:47 +0330

    update using fastpush

--- resources/views/notif.blade.php
+++ resources/views/notif.blade.php
@@ -16,7 +16,29 @@
 </div>
 
 <script>
+'use strict';
 
+const applicationServerPublicKey = 'BPzpEiZerN0INBDvqd8cgUaXvn6_UVt8F0A1eLDb18YkpFno6YvIsZFreSOtW5Izq_CN23yBgdz7UFc7FxlzKFY';
+
+const pushButton = document.querySelector('.js-push-btn');
+
+let isSubscribed = false;
+let swRegistration = null;
+
+function urlB64ToUint8Array(base64String) {
+  const padding = '='.repeat((4 - base64String.length % 4) % 4);
+  const base64 = (base64String + padding)
+    .replace(/\-/g, '+')
+    .replace(/_/g, '/');
+
+  const rawData = window.atob(base64);
+  const outputArray = new Uint8Array(rawData.length);
+
+  for (let i = 0; i < rawData.length; ++i) {
+    outputArray[i] = rawData.charCodeAt(i);
+  }
+  return outputArray;
+}
 
 if ('serviceWorker' in navigator && 'PushManager' in window) {
   console.log('Service Worker and Push is supported');
